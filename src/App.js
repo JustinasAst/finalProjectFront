4781f8bc1b93088723286c1fc36b5cba876656d8
Justinas Astrauskas <jst.astrauskas@gmail.com>
2021-11-15 19:53:31 +0200

    Fix lazyLoading

--- src/App.js
+++ src/App.js
@@ -1,40 +1,26 @@
 import './App.css';
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Home, Register, Login, Add } from './pages';
+/* import { Home, Register, Login, Add } from './pages'; */
 import PrivateRoute from './components/PrivateRoute';
 
-/* const LazyHome = lazy(() => import('./pages/Home/Home'));
-const LazyRegister = lazy(() => import('./pages/Register/Register'));
-const LazyLogin = lazy(() => import('./pages/Login/Login'));
-const lazyAdd = lazy(() => import('./pages/add/Add'));
- */
+const LazyHome = React.lazy(() => import('./pages/Home/Home'));
+const LazyRegister = React.lazy(() => import('./pages/Register/Register'));
+const LazyLogin = React.lazy(() => import('./pages/Login/Login'));
+const LazyAdd = React.lazy(() => import('./pages/add/Add'));
+
 function App() {
 	return (
 		<div className='App'>
 			<Router>
-				<Routes>
-					<Route exact path='/register' element={<Register />} />
-					<Route exact path='/login' element={<Login />} />
-					<Route
-						exact
-						path='/'
-						element={
-							<PrivateRoute>
-								<Home />
-							</PrivateRoute>
-						}
-					/>
-					<Route
-						exact
-						path='/add'
-						element={
-							<PrivateRoute>
-								<Add />
-							</PrivateRoute>
-						}
-					/>
-				</Routes>
+				<Suspense fallback={() => <h1>Loading...</h1>}>
+					<Routes>
+						<Route exact path='/register' element={<LazyRegister />} />
+						<Route exact path='/login' element={<LazyLogin />} />
+						<Route exact path='/' element={<PrivateRoute>{<LazyHome />}</PrivateRoute>} />
+						<Route exact path='/add' element={<PrivateRoute>{<LazyAdd />}</PrivateRoute>} />
+					</Routes>
+				</Suspense>
 			</Router>
 		</div>
 	);
